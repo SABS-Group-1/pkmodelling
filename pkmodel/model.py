08cce98a2e0c4083cfe6be0aeed7c9a242598e5e
Leo Klarner <leo.klarner@bnc.ox.ac.uk>
2020-10-21 17:04:43 +0200

    Created a solver method by copying the code from the protocol.py file.

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -2,6 +2,23 @@
 # Model class
 #
 
+import matplotlib.pylab as plt
+import numpy as np
+import scipy.integrate
+
+
+def dose(t, X):
+    return X
+
+
+def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
+    q_c, q_p1 = y
+    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
+    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
+    dqp1_dt = transition
+    return [dqc_dt, dqp1_dt]
+
+
 class Model:
     """A Pharmokinetic (PK) model
 
@@ -11,7 +28,7 @@ class Model:
         an example paramter
     """
 
-    def __init__(self, model_type="IB", compartment_no=1, CL=1.0, Vc=1.0, Vp1=0, Qp1=0, X=1):
+    def __init__(self, model_type="IB", compartment_no=1, CL=1.0, V_c=1.0, V_p1=1, Q_p1=1, X=1):
         """
         Initialisation of the model class, fixed for one compartment and a discrete initial dose for now
 
@@ -27,8 +44,26 @@ class Model:
         self.model_type = model_type  # one of two str (IB, SC)
         self.compartment_no = compartment_no # should be positive int
         self.CL = CL # should be >0
-        self.Vc = Vc # should be >0
+        self.V_c = V_c # should be >0
         # possible for loop / list for later
-        self.Vp1 = Vp1 # should be >0
-        self.Qp1 = Qp1 # should be >0
+        self.V_p1 = V_p1 # should be >0
+        self.Q_p1 = Q_p1 # should be >0
         self.X = X # should be >0
+
+
+    def solve(self):
+        t_eval = np.linspace(0, 1, 10)
+        y0 = np.array([0.0, 0.0])
+
+        sol = scipy.integrate.solve_ivp(
+            fun=lambda t, y: rhs(t, y, self.Q_p1, self.V_c, self.V_p1, self.CL, self.X),
+            t_span=[t_eval[0], t_eval[-1]],
+            y0=y0, t_eval=t_eval
+        )
+
+        return sol
+
+
+if __name__ == "__main__":
+    model = Model()
+    print(model.solve())
