e88d0c5788136e45b741e3f65afcb192b239e51d
lewis-chinery <lewis.chinery@dtc.ox.ac.uk>
2020-10-22 11:13:32 +0100

    Add initial default value tests to test_model.py

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -6,10 +6,31 @@ class ModelTest(unittest.TestCase):
     """
     Tests the :class:`Model` class.
     """
-    def test_create(self):
+    def test_default_init_attribute_values(self):
         """
         Tests Model creation.
         """
-        model = pk.Model()
-        self.assertEqual(model.value, 42)
+        test_model = pk.Model()
+        default_values = {"clearance_rate": 1,
+                        "vol_c": 1,
+                        "dose": 1,
+                        "subcutaneous_compartment": None,
+                        "peripheral_compartments": [],
+                        "number_of_compartments": 1,
+                        "number_of_peripheral_compartments": 0
+                        }
+        assert(test_model.clearance_rate == default_values["clearance_rate"])
+        assert(test_model.vol_c == default_values["vol_c"])
+        assert(test_model.dose == default_values["dose"])
+        assert(test_model.subcutaneous_compartment == default_values["subcutaneous_compartment"])
+        assert(test_model.peripheral_compartments == default_values["peripheral_compartments"])
+        assert(test_model.number_of_compartments == default_values["number_of_compartments"])
+        assert(test_model.number_of_peripheral_compartments == default_values["number_of_peripheral_compartments"])
+        
 
+
+
+        
+
+
+        
