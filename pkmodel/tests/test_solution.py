7f23f2a04a988d4e3d02f95744bc1982821294cb
Leo Klarner <leo.klarner@bnc.ox.ac.uk>
2020-10-22 16:40:08 +0200

    Push for Lewis to see parametrized library.

--- pkmodel/tests/test_solution.py
+++ pkmodel/tests/test_solution.py
@@ -34,3 +34,32 @@ class SolutionTest(TestCase):
                 solution.solve(y0=y0)
         else:
             solution.solve(y0=y0)
+
+    @parameterized.expand([
+        ('model1', 1.0, 1.0, 1.0, 1.0, 1.0),
+        ('model2', 2.0, 1.0, 1.0, 1.0, 1.0)
+    ])
+    def prototypeResults(self, name, Q_p1, V_c, V_p1, CL, X):
+        """
+        Inputs the parameter sets from the prototype class and checks whether
+        our programme produces the same output.
+        :param name: name of our model
+        :param Q_p1: transfer rate
+        :param V_c: volume of the central compartment
+        :param V_p1: volume of the peripheral compartment
+        :param CL: clearance rate
+        :param X: drug dosing
+        :return:
+        """
+        from pkmodel.model import Model
+        from pkmodel.solution import Solution
+
+        model = Model(clearance_rate=CL, vol_c=V_c, dose=X)
+        model.add_peripheral_compartment(vol_p=V_p1, q_p=Q_p1)
+
+        solution = Solution(model)
+        result = solution.solve(y0=np.array([0.0, 0.0]), t_eval=np.linspace(0, 1, 1000))
+
+
+
+
