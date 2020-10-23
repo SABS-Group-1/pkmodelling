b77669c422b3c6963ff6394e28b3455153a24454
lewis-chinery <lewis.chinery@dtc.ox.ac.uk>
2020-10-23 10:02:30 +0100

    #31 Relace double line breaks with single in test_model.py for flake8 styling

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,6 +1,5 @@
 import unittest
 import pkmodel as pk
-import numpy as np
 from parameterized import parameterized
 
 
@@ -14,13 +13,13 @@ class ModelTest(unittest.TestCase):
         """
         test_model = pk.Model()
         default_values = {"clearance_rate": 1,
-                        "vol_c": 1,
-                        "dose": 1,
-                        "subcutaneous_compartment": None,
-                        "peripheral_compartments": [],
-                        "number_of_compartments": 1,
-                        "number_of_peripheral_compartments": 0
-                        }
+                          "vol_c": 1,
+                          "dose": 1,
+                          "subcutaneous_compartment": None,
+                          "peripheral_compartments": [],
+                          "number_of_compartments": 1,
+                          "number_of_peripheral_compartments": 0
+                         }
         assert(test_model.clearance_rate == default_values["clearance_rate"])
         assert(test_model.vol_c == default_values["vol_c"])
         assert(test_model.dose == default_values["dose"])
@@ -28,7 +27,6 @@ class ModelTest(unittest.TestCase):
         assert(test_model.peripheral_compartments == default_values["peripheral_compartments"])
         assert(test_model.number_of_compartments == default_values["number_of_compartments"])
         assert(test_model.number_of_peripheral_compartments == default_values["number_of_peripheral_compartments"])
-
 
     def test_add_peripheral_compartments_increases_compartments(self):
         """
@@ -45,7 +43,6 @@ class ModelTest(unittest.TestCase):
 
         for i in range(len(initial_compartments)): 
             assert(new_compartments[i] == initial_compartments[i] +1)
-
 
     def test_vol_and_quantity_peripheral_compartments(self):
         '''
@@ -64,7 +61,6 @@ class ModelTest(unittest.TestCase):
         assert(test_model.peripheral_compartments[1]["vol_p"] == vol_p)
         assert(test_model.peripheral_compartments[1]["q_p"] == q_p)
 
-
     def test_subcutaneous_compartment_equals_absorption(self):
         '''
         Test subcutaneous compartment equals absorption rate
@@ -82,7 +78,6 @@ class ModelTest(unittest.TestCase):
         test_model.add_subcutaneous_compartment(absorption_rate = abs_rate)
         assert(test_model.subcutaneous_compartment == abs_rate)
 
-
     def test_add_subcutaneous_compartments_increases_compartments(self): 
         '''
         Test add_subcutaneous_compartments adds 1 to object.no_of_compartments
@@ -92,7 +87,6 @@ class ModelTest(unittest.TestCase):
 
         test_model.add_subcutaneous_compartment()
         assert(test_model.number_of_compartments == initial_no_of_compartments + 1)
-
             
     def test_cannot_add_more_than_1_sc_compartment(self):
         '''
@@ -106,7 +100,6 @@ class ModelTest(unittest.TestCase):
 
         with self.assertRaises(AttributeError):
             test_model.add_subcutaneous_compartment()
-
 
     @parameterized.expand([
         ([100], TypeError),
@@ -125,25 +118,23 @@ class ModelTest(unittest.TestCase):
         with self.assertRaises(expected):
             test_model = pk.Model(dose = input)
 
-
     @parameterized.expand([
-        (-1, ValueError)
-        ])
+                          (-1, ValueError)
+                          ])
     def test_default_values_positivity(self, input, expected):
         '''
         Test ValueErrors are raised for invalid model params
         '''
         with self.assertRaises(expected):
-            test_model = pk.Model(clearance_rate = input)
+            test_model = pk.Model(clearance_rate=input)
         with self.assertRaises(expected):
-            test_model = pk.Model(vol_c = input)
+            test_model = pk.Model(vol_c=input)
         with self.assertRaises(expected):
-            test_model = pk.Model(dose = input)
+            test_model = pk.Model(dose=input)
         #with self.assertRaises(expected):
         #    test_model2 = pk.Model()
         #    test_model2.number_of_compartments = -1
         #    test_model2.number_of_compartments = input
         #with self.assertRaises(expected):
         #    test_model2.number_of_peripheral_compartments = input
-    
         #missing a couple of initial parameters (None and list)
