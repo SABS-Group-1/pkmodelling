e519fc85787e33f1156e54a54ee7acd1e9c9fda5
Ewa Wieczorek <72970328+ewawieczorek@users.noreply.github.com>
2020-10-23 14:07:45 +0100

    Fix style issues in demo.py

--- demo.py
+++ demo.py
@@ -5,8 +5,8 @@ model = pk.model.Model(clearance_rate=1, vol_c=1, dose=1)
 
 #the starting model only has a central compartment and assumes intravenous dosing
 #to change this model use the functions below
-model.add_subcutaneous_compartment(absorption_rate=2) #needed if the dosing protocol is subcutaneous
-model.add_peripheral_compartment(pc_name="Compartment 1", vol_p=2, q_p=3) #adds a peripheral compartment, can be repeated multiple times
+model.add_subcutaneous_compartment(absorption_rate=2)  #needed if the dosing protocol is subcutaneous
+model.add_peripheral_compartment(pc_name="Compartment 1", vol_p=2, q_p=3)  #adds a peripheral compartment
 model.add_peripheral_compartment()
 
 sol = pk.solution.Solution(model=model)
