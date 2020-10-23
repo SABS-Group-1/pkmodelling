035fb66fa6d691204de0446af5e2b165bfea3c78
Emily Thomas <esthomas09@hotmail.com>
2020-10-23 10:25:16 +0100

    #31 sorted flake8 style errors for model.py

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -13,12 +13,15 @@ class Model:
         """
         Initialise model object param values
 
-        :param clearance_rate: int/float describing drug elimination rate from central compartment [ml/h]
+        :param clearance_rate: int/float describing drug elim rate
+                               from central compartment [ml/h]
         :param vol_c: int/float describing volume of central compartment [ml]
         :param dose: int/float dosing function [ng/h]
-        :arg subcutaenous_compartment: int/float describing absortion rate for SC dosing [/h]. Assume IVB model so defaults to None
+        :arg subcutaenous_compartment: int/float describes absorption rate [/h]
+                                       Assume IVB model so defaults to None
         :arg peripheral_compartments: initialised as empty list
-        :arg number_of_compartments: int initialised to 1 for central compartment only
+        :arg number_of_compartments: int initialised to 1
+                                     for central compartment only
         :arg number_of_peripheral_compartments: int initialised to 0
         :return: ---
         """
@@ -30,40 +33,41 @@ class Model:
         self.number_of_compartments = 1
         self.number_of_peripheral_compartments = 0
 
-        # check model param values are physically valid 
+        # check model param values are physically valid
         if self.clearance_rate <= 0 or self.vol_c <= 0 or self.dose <= 0:
             raise ValueError("Inputted negative number")
 
         # check model param types
-        if not isinstance(self.clearance_rate, (int,float)):
+        if not isinstance(self.clearance_rate, (int, float)):
             raise TypeError("Clearance rate must be an int or float")
-        if not isinstance(self.vol_c, (int,float)):
+        if not isinstance(self.vol_c, (int, float)):
             raise TypeError("Central volume must be an int or float")
-        if not isinstance(self.dose, (int,float)):
+        if not isinstance(self.dose, (int, float)):
             raise TypeError("Dose must be an int or float")
-        if not (isinstance(self.subcutaneous_compartment, (int,float)) or self.subcutaneous_compartment == None):
-            raise TypeError("Subcutaneous compartment absorption rate must be None, int, or float")
+        if not (isinstance(self.subcutaneous_compartment, (int, float))
+                or self.subcutaneous_compartment is None):
+            raise TypeError("SC compart. abs. rate must be None, int or float")
         if not isinstance(self.peripheral_compartments, list):
-            raise TypeError("Peripheral compartment data must be in the form of a list")
+            raise TypeError("Peripheral compartment data must be list")
         if not isinstance(self.number_of_compartments, int):
             raise TypeError("Total number of compartments must be an int")
         if not isinstance(self.number_of_peripheral_compartments, int):
-            raise TypeError("Total number of peripheral compartments must be an int")
+            raise TypeError("Total no. of peripheral compartments must be int")
 
     def add_subcutaneous_compartment(self, absorption_rate=1):
         '''
         Adds subcutaneous compartment to model with absorption rate specified
 
-        :param absorption_rate: int/float describing absortion rate for SC dosing [/h]
+        :param absorption_rate: int/float describing absorp. rate for SC [/h]
         :return: ---
         '''
         if self.subcutaneous_compartment:
-            raise AttributeError("There can only be one subcutaneous compartment.")
+            raise AttributeError("There can only be one SC compartment")
         else:
             self.subcutaneous_compartment = absorption_rate
             self.number_of_compartments += 1
 
-    def add_peripheral_compartment(self, pc_name = None, vol_p=1, q_p=1):
+    def add_peripheral_compartment(self, pc_name=None, vol_p=1, q_p=1):
         '''
         Adds peripheral compartment to model
         Valid for both IVB and SC
@@ -73,8 +77,10 @@ class Model:
         :return: ---
         '''
         if not pc_name:
-            pc_name = "Peripheral Compartment {}".format(self.number_of_peripheral_compartments + 1)
-        self.peripheral_compartments.append({"name": pc_name, "vol_p":vol_p, "q_p": q_p})
+            pc_name = "Peripheral Compartment {}".format(
+                self.number_of_peripheral_compartments + 1)
+        self.peripheral_compartments.append(
+            {"name": pc_name, "vol_p": vol_p, "q_p": q_p})
         self.number_of_compartments += 1
         self.number_of_peripheral_compartments += 1
 
@@ -85,5 +91,8 @@ if __name__ == "__main__":
     model.add_peripheral_compartment('some cell', 21, 22)
     model.add_peripheral_compartment()
     for i in range(model.number_of_peripheral_compartments):
-        print("{}\t|\t vol: {}\t|\t drug quant: {}".format(model.peripheral_compartments[i]["name"], model.peripheral_compartments[i]["vol_p"], model.peripheral_compartments[i]["q_p"]))
+        print("{}\t|\t vol: {}\t|\t drug quant: {}".format(
+            model.peripheral_compartments[i]["name"],
+            model.peripheral_compartments[i]["vol_p"],
+            model.peripheral_compartments[i]["q_p"]))
     print(model.peripheral_compartments)
