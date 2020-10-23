4d5144a7cd5c2668aa6a8f17291add6fc0cdf6b5
Ewa Wieczorek <72970328+ewawieczorek@users.noreply.github.com>
2020-10-23 14:04:06 +0100

    Add descriptions into demo.py

--- demo.py
+++ demo.py
@@ -3,8 +3,10 @@ import pkmodelling_sabs_group1 as pk  # if the package is installed through pip
 
 model = pk.model.Model(clearance_rate=1, vol_c=1, dose=1)
 
-model.add_subcutaneous_compartment(absorption_rate=2)
-model.add_peripheral_compartment(pc_name="Compartment 1", vol_p=2, q_p=3)
+#the starting model only has a central compartment and assumes intravenous dosing
+#to change this model use the functions below
+model.add_subcutaneous_compartment(absorption_rate=2) #needed if the dosing protocol is subcutaneous
+model.add_peripheral_compartment(pc_name="Compartment 1", vol_p=2, q_p=3) #adds a peripheral compartment, can be repeated multiple times
 model.add_peripheral_compartment()
 
 sol = pk.solution.Solution(model=model)
