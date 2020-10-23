62beee296f792b0e3241a8e0d0a2f7b57fa13706
Ewa Wieczorek <ewa.wieczorek@gtc.ox.ac.uk>
2020-10-23 15:33:28 +0100

    Create dosing protocol class

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -12,6 +12,41 @@ class Protocol:
         an example paramter
 
     """
-    def __init__(self, value=43):
-        self.value = value
+    def __init__(self, dose_amount=1,
+                 continuous=False, continuous_period=[0, 0],
+                 instantaneous=True, dose_times=[0]):
+        self.dose_amount = dose_amount
+        self.continuous = continuous
+        self.instantaneous = instantaneous
+        self.dose_times = dose_times
+        self.continuous_period = continuous_period
+        self.dose_times.sort()
+
+    def make_continuous(self,time_start, time_finish):
+        self.continuous = True
+        self.continuous_period[0] = time_start
+        self.continuous_period[1] = time_finish
+    
+    def add_instantaneous(self, time):
+        self.dose_times.append(time)
+        self.dose_times.sort()
+    
+    def change_dose(self, dose_amount):
+        self.dose_amount = dose.amount
+    
+    def dose_at_time(self, t):
+        dose_t_continuous, dose_t_instantaneous = 0, 0
+        if self.instantaneous:
+            if t in self.dose_times:
+                dose_t_instantaneous = self.dose_amount
+
+        if self.continuous:
+            if t < self.continuous_period[1] and \
+               t >= self.continuous_period[0]:
+                dose_t_continuous = self.dose_amount
+
+        dose_t = dose_t_continuous + dose_t_instantaneous
+        return dose_t
+
+
 
